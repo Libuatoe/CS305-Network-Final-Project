2663fcd5f0326b703c8f83a636f888dae5be265b
Libuatoe <ousiqiuseqian@gmail.com>
2020-12-11 14:56:20 +0800

    temp commit

--- Client_Test.py
+++ Client_Test.py
@@ -1,3 +1,7 @@
 from rdt import RDTSocket
 
-server = RDTSocket(rate=None)
+if __name__ == '__main__':
+    client_address = ('127.0.0.1', 44444)
+    client = RDTSocket(rate=None)
+    client.bind(client_address)
+
